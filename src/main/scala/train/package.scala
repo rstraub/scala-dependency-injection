281236328bb36d0c7485042c1a93d12cfecd8ee4
Roy Straub <roy.straub@codesquad.nl>
2022-02-07 10:09:40 +0100

    Coupling

--- src/main/scala/train/package.scala
+++ src/main/scala/train/package.scala
@@ -2,12 +2,15 @@ package object train {
   class PointSwitcher() {
     def switch(): Unit = println("switching")
   }
-  class TrainCarCoupler()
+  class TrainCarCoupler() {
+    def couple(): Unit = println("coupling")
+  }
   class TrainShunter(pointSwitcher: PointSwitcher,
                      trainCarCoupler: TrainCarCoupler) {
     def shunt(): Unit = {
       println("shunting")
       pointSwitcher.switch()
+      trainCarCoupler.couple()
     }
   }
 
